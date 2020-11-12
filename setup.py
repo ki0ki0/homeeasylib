0e31de29a9c2d6fe8c0854dcb868900cf4d9bf23
Ihor Kostiuk <igorkost@gmail.com>
2020-11-12 21:15:06 +0200

    version 0.0.1 goes public

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-  long_description = fh.read()
+import codecs
+import os.path
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 setuptools.setup(
   name="homeeasy",
-  version="0.0.2",
+  version=get_version("homeeasy/__init__.py"),
   description="Control HVAC devices compatible with HomeEasy application.",
-  long_description=long_description,
+  long_description=read("README.md"),
   long_description_content_type="text/markdown",
   url="https://github.com/ki0ki0/homeeasylib",
   author="Ihor Kostiuk",
